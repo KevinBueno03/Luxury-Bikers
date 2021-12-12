c8553910af8540952580b8ef196f78a67b52130c
KevinBueno03 <kbueno@unah.hn>
2021-12-12 05:05:54 -0600

    modals

--- Bikers/src/app/pages/orders/orders.component.ts
+++ Bikers/src/app/pages/orders/orders.component.ts
@@ -12,12 +12,7 @@ import { environment } from 'src/environments/environment';
 })
 export class OrdersComponent implements OnInit {
 
-  mapa!: mapboxgl.Map;
-  data= JSON.stringify('all');
-  token= JSON.stringify(localStorage.getItem("token"));
-  longitud = -87.207;
-  latitud = 14.0383;
-  jsonLngLat: any;
+ 
 
   biker: any;
   ordenesPagadas:any;
@@ -49,35 +44,14 @@ export class OrdersComponent implements OnInit {
       }
     );
 
-    (mapboxgl as any).accessToken = environment.mapBoxKey;
-    this.mapa = new mapboxgl.Map({
-      container: 'mapbox',
-      style: 'mapbox://styles/mapbox/streets-v11',
-      center: [this.longitud, this.latitud],
-      zoom: 15
-    });
-    this.crearMarcador(this.longitud, this.latitud);
   }
 
-  crearMarcador(longitud: number, latitud: number) {
-    const marcador = new mapboxgl.Marker({
-      draggable: true
-    }).setLngLat([longitud, latitud])
-      .addTo(this.mapa);
-
-    this.jsonLngLat=marcador.getLngLat();
-    marcador.on('drag', () => {
-      this.jsonLngLat=marcador.getLngLat();
-      this.longitud=marcador.getLngLat().lng;
-      this.latitud=marcador.getLngLat().lat;
-    })
-  }
-
+ 
   openAsignarMotorista(modal:any,idOrder:any,orden:any){
 
     this.idOrdenSeleccionada=idOrder;
     this.orden= orden;
-    this.crearMarcador(this.orden.location.lng,this.orden.location.lat)
+    
     this.modalService.open(
       modal,
       {
@@ -90,13 +64,8 @@ export class OrdersComponent implements OnInit {
   }
 
   asignarMotoristaAOrden(){
-<<<<<<< Updated upstream
-
-    this.body={idBiker:this.biker._id,taked:true};
-=======
   
     this.body={idBiker:this.biker._id,taked:true,orderStatus:"Tomada"};
->>>>>>> Stashed changes
     this.orderService.updateOrder(this.idOrdenSeleccionada,this.body).subscribe(res=>{
     if(res){
       this.orderService.obtenerOrdenesPagadasSinTomar().subscribe(
--- Bikers/src/app/pages/ship-order/ship-order.component.ts
+++ Bikers/src/app/pages/ship-order/ship-order.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { BikerService } from 'src/app/services/biker.service';
+import { OrderService } from 'src/app/services/order.service';
 
 @Component({
   selector: 'app-ship-order',
@@ -7,9 +10,73 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShipOrderComponent implements OnInit {
 
-  constructor() { }
+ 
+  biker: any;
+  ordenesPagadas:any;
+
+  idOrdenSeleccionada:any;
+  orden:any;
+  body:any;
+  constructor(
+    private modalService:NgbModal,private bikerService : BikerService, private orderService: OrderService) { }
 
   ngOnInit(): void {
+
+    this.bikerService.obtenerBiker().subscribe(
+      res =>{
+        console.log(res);
+        this.biker=res;
+        console.log(this.biker);
+      },error =>{
+        console.log(error);
+      }
+    )
+
+    this.orderService.obtenerOrdenesPagadasSinTomar().subscribe(
+      res =>{
+        this.ordenesPagadas=res;
+      },
+      error =>{
+        console.log(error);
+      }
+    );
+
+  }
+
+  
+
+  openAsignarMotorista(modal:any,idOrder:any,orden:any){
+
+    this.idOrdenSeleccionada=idOrder;
+    this.orden= orden;
+    
+    this.modalService.open(
+      modal,
+      {
+        size:'xl',
+        centered:true
+      }
+    );
+
+
+  }
+  asignarMotoristaAOrden(){
+  
+    this.body={idBiker:this.biker._id,taked:true,orderStatus:"Tomada"};
+    this.orderService.updateOrder(this.idOrdenSeleccionada,this.body).subscribe(res=>{
+    if(res){
+      this.orderService.obtenerOrdenesPagadasSinTomar().subscribe(
+        res =>{
+          this.ordenesPagadas=res;
+        }
+        ,
+        error =>{
+          console.log(error);
+        }
+      )
+      }
+    });
+
   }
 
 }
